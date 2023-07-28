ef58d3b98a08fdf54e3b9481e0f8f4857532e4ff
TheCamBloch <129989249+TheCamBloch@users.noreply.github.com>
2023-07-28 15:28:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ while display.IsStreaming():
         img = camera.Capture()
         ScaledUp = jetson.utils.cudaAllocMapped(width=ScreenSize[0], height=ScreenSize[1], format=img.format)
         jetson.utils.cudaResize(img, ScaledUp)
+        jetson.utils.cudaDeviceSynchronize()
         detections = net.Detect(ScaledUp)
         display.Render(ScaledUp)
-        jetson.utils.cudaDeviceSynchronize()
         display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
         for detection in detections:
                 if net.GetClassDesc(detection.ClassID) == "index":
